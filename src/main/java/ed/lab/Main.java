42863a388954665f569cf93501f40e9c13a61b79
marcs <mwschaub@correo.url.edu.gt>
2025-02-28 03:35:31 -0600

    quicksort push

--- src/main/java/ed/lab/Main.java
+++ src/main/java/ed/lab/Main.java
@@ -22,16 +22,16 @@ public class Main {
         Random rand = new Random();
         Integer[] arr = new Integer[length];
         for (int i = 0; i < length; i++) {
-            arr[i] = rand.nextInt(1000); // aquí use de limite 1000
+            arr[i] = rand.nextInt(100); // aquí use de limite 100
         }
         return arr;
     }; // Reemplácelo por una función lambda crear un array completamente aleatorio
 
-    private static final QuickSort<Integer> highPivotQuickSort = null; // Reemplácelo por una referencia a un método
+    private static final QuickSort<Integer> highPivotQuickSort = SortingAlgorithms::highPivotQuickSort;; // Reemplácelo por una referencia a un método
 
-    private static final QuickSort<Integer> lowPivotQuickSort = null; // Reemplácelo por una referencia a un método
+    private static final QuickSort<Integer> lowPivotQuickSort = SortingAlgorithms::lowPivotQuickSort;; // Reemplácelo por una referencia a un método
 
-    private static final QuickSort<Integer> randomPivotQuickSort = null; // Reemplácelo por una referencia a un método
+    private static final QuickSort<Integer> randomPivotQuickSort = SortingAlgorithms::randomPivotQuickSort; // Reemplácelo por una referencia a un método
 
     public static QuickSort<Integer> getHighPivotQuickSort() {
         return highPivotQuickSort;
--- src/main/java/ed/lab/SortingAlgorithms.java
+++ src/main/java/ed/lab/SortingAlgorithms.java
@@ -1,5 +1,55 @@
 package ed.lab;
 
-public class SortingAlgorithms {
+import java.util.Random;
 
+public class SortingAlgorithms {
+    public static <T extends Comparable<T>> void highPivotQuickSort(T[] array) {
+        quickSort(array, 0, array.length - 1, "high");
+    }
+
+    public static <T extends Comparable<T>> void lowPivotQuickSort(T[] array) {
+        quickSort(array, 0, array.length - 1, "low");
+    }
+
+    public static <T extends Comparable<T>> void randomPivotQuickSort(T[] array) {
+        quickSort(array, 0, array.length - 1, "random");
+    }
+
+    private static <T extends Comparable<T>> void quickSort(T[] array, int left, int right, String pivotType) {
+        if (left < right) {
+            int pivotIndex;
+            if ("high".equals(pivotType)) {
+                pivotIndex = right;
+            } else if ("low".equals(pivotType)) {
+                pivotIndex = left;
+            } else {
+                pivotIndex = new Random().nextInt(right - left + 1) + left; // Aleatorio
+            }
+
+            pivotIndex = partition(array, left, right, pivotIndex);
+            quickSort(array, left, pivotIndex - 1, pivotType);
+            quickSort(array, pivotIndex + 1, right, pivotType);
+        }
+    }
+
+    private static <T extends Comparable<T>> int partition(T[] array, int left, int right, int pivotIndex) {
+        T pivotValue = array[pivotIndex];
+        swap(array, pivotIndex, right);
+        int storeIndex = left;
+
+        for (int i = left; i < right; i++) {
+            if (array[i].compareTo(pivotValue) < 0) {
+                swap(array, i, storeIndex);
+                storeIndex++;
+            }
+        }
+        swap(array, storeIndex, right);
+        return storeIndex;
+    }
+
+    private static <T> void swap(T[] array, int i, int j) {
+        T temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
